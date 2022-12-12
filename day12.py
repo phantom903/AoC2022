97a342ca70ae806fabdbcc0e0b3885deed341645
Dave Ponting <pontingdavid.81@gmail.com>
2022-12-12 14:45:25 +0000

    day 12

---
+++ day12.py
@@ -0,0 +1,45 @@
+from aoc import *
+import sys, collections
+
+def solve(grid, *start):
+    Q = collections.deque((i, j, 0, 'a') for i in range(len(grid)) 
+                    for j in range(len(grid[0])) 
+                    if grid[i][j] in start)
+    visited = set((i, j) for i, j, _, _ in Q)
+
+    def push(i, j, d, a):
+        if not (0 <= i < len(grid)) or not (0 <= j < len(grid[0])): return
+        if (i, j) in visited: return
+        b = grid[i][j].replace('E', 'z')
+        if ord(b) > ord(a) + 1: return
+        visited.add((i, j))
+        Q.append((i, j, d + 1, b))
+
+    while len(Q):
+        i, j, d, a = Q.popleft()
+        if grid[i][j] == 'E': return d
+        push(i + 1, j, d, a)
+        push(i - 1, j, d, a)
+        push(i, j + 1, d, a)
+        push(i, j - 1, d, a)
+
+class DayTwelve:
+
+  mem = []
+  start = None
+  end = None
+
+  def __init__(self, mem):
+    self.mem = mem.copy()
+
+  def partOne(self):
+    return solve(self.mem, 'S')
+
+  def partTwo(self):
+    return solve(self.mem, 'S', 'a')
+
+if __name__ == "__main__":
+  y = fileOpenLines(12)
+  dayTwelve = DayTwelve(y)
+  print('Day Twelve part one : ', dayTwelve.partOne())
+  print('Day Twelve part two : ', dayTwelve.partTwo())
