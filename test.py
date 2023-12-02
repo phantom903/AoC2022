95e2416289efdc2a0beb8777a1ec054e2c361499
phantom903 <pontingdavid.81@gmail.com>
2023-12-02 15:08:56 +0000

    x

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
-import tkinter as tk
+import os 
+import sys
 
-window = tk.Tk()
-window.rowconfigure([0, 1, 2, 3, 4, 5], minsize = 20)
-window.columnconfigure([x for x in range(40)], minsize = 16)
-label1 = tk.Label(text = '', bg = 'black', fg = 'white')
-label1.grid(row = 0, column = 0, sticky = 'nsew')
-window.mainloop()
+def console_frame(output):
+	os.system('clear' if os.name == 'posix' else 'CLS')
+	sys.stdout.write(output + "\n")
+	sys.stdout.flush()
+
+
+import time
+import math
+
+for t in range(100):
+	console_frame("\n".join(["*" * (30 + int(30 * math.sin(.1 * x + .1 * t))) for x in range(30)])) # time-varying sine wave
+	time.sleep(.04)
